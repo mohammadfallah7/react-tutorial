668bc0b8a558abe20b3528ead8a7a37d4c9bfc97
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-07-02 19:43:39 +0330

    feat: :sparkles: Install and config react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import "./index.css";
 import router from "./router.tsx";
 
+const client = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
